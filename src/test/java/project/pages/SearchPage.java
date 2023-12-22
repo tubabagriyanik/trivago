c72eecf0ad78b40977c7349225c9552e2e415b4f
Demetic <iclekdemet@gmail.com>
2023-12-22 12:41:01 -0500

    after locate info

--- src/test/java/project/pages/SearchPage.java
+++ src/test/java/project/pages/SearchPage.java
@@ -6,21 +6,35 @@ import org.openqa.selenium.support.FindBy;
 public class SearchPage extends BasePage{
     @FindBy(xpath = "//input[@autocorrect='off']")
     public WebElement destination;
+    @FindBy(xpath = "//input[@data-testid='search-form-destination']")
+    public WebElement destinationButton;
     @FindBy(xpath = "//button[@data-testid='search-form-calendar-checkin']")
-        public WebElement checkInClick;
+    public WebElement checkInClick;
     @FindBy(xpath = "//button[@data-testid='search-form-calendar-checkout']")
     public WebElement checkOutClick;
-    @FindBy(xpath = "//span[@class='inline-flex leading-none -rotate-90 transform transform']")
+    @FindBy(xpath = "//button[@name='property_type']")
     public WebElement propertyType;
-
     @FindBy(xpath = "//li[@data-testid='accommodation-type-filter-entire house / apartment']")
     public WebElement entireHouseCheckbox;
-     @FindBy(xpath = "//button[@data-testid='filters-popover-apply-button']")
+    @FindBy(xpath = "//button[@data-testid='filters-popover-apply-button']")
     public WebElement applyButton;
+    @FindBy(xpath = "//button[@data-testid='item-name']")
+    public WebElement firstHouseTitle;
+    @FindBy(xpath = "//span[@data-testid='price-label']")
+    public WebElement priceTravel;
+    @FindBy(id = "tabs-296-tab-0")
+    public WebElement overview;
+    @FindBy(xpath = "//button[@id='tabs-293-tab-1']")
+    public WebElement info;
+    @FindBy(id = "tabs-343-tab-2")
+    public WebElement photos;
+    @FindBy(id = "tabs-343-tab-3")
+    public WebElement reviews;
+    @FindBy(id = "tabs-343-tab-4")
+    public WebElement price;
+    @FindBy(xpath= "//article[@data-testid='amenities-module']")
+    public WebElement topAmenities;
 
-
-   @FindBy(xpath = "//span[text()='Montréal']")
-        public WebElement montreal;
 
 
 }
