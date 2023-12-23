ba5773d484e37936fdceaaaa6bc5f49a5f15d4d4
Betul <127248246+betulgulbahce@users.noreply.github.com>
2023-12-22 19:51:43 -0500

    edited feature file

---
+++ src/test/java/project/stepDefinitions/US_09_Error_Handling.java
@@ -0,0 +1,65 @@
+package project.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US_09_Error_Handling {
+
+//    @Given("User navigates to url")
+//    public void user_navigates_to_url() {
+//
+//    }
+    @Given("User clicks on search query box")
+    public void user_clicks_on_search_query_box() {
+
+    }
+    @When("User enters invalid data")
+    public void user_enters_invalid_data() {
+
+    }
+    @Then("User should see the error message.")
+    public void user_should_see_the_error_message() {
+
+    }
+
+
+
+//    @Given("User navigates to url")
+//    public void user_navigates_to_url() {
+//
+//    }
+    @Given("User enters a destination on home page")
+    public void user_enters_a_destination_on_home_page() {
+
+    }
+    @When("user clicks on search button")
+    public void user_clicks_on_search_button() {
+
+    }
+    @When("user clicks on View Deal button of the Hotel information box")
+    public void user_clicks_on_view_deal_button_of_the_hotel_information_box() {
+
+    }
+    @When("website switches to another tab")
+    public void website_switches_to_another_tab() {
+
+    }
+    @When("user selects a room and clicks on Quick Book button")
+    public void user_selects_a_room_and_clicks_on_quick_book_button() {
+
+    }
+    @When("clicks Continue button on the pop-up message box")
+    public void clicks_continue_button_on_the_pop_up_message_box() {
+
+    }
+    @When("user enters invalid card number")
+    public void user_enters_invalid_card_number() {
+
+    }
+    @Then("user should see the error message under card number box")
+    public void user_should_see_the_error_message_under_card_number_box() {
+
+    }
+
+}
